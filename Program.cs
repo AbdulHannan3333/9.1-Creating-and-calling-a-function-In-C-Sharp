7b80088e83ce1cd68feb3d72dd2270f3ed9a5e30
AbdulHannan3333 <hannan67212@outlook.com>
2024-06-19 20:15:20 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9._1_Creating_and_calling_a_function
+{
+    internal class Program
+    {
+        static void greet() 
+        {
+            Console.WriteLine("Hello , there");
+        }
+        static void Main(string[] args)
+        {
+            greet();
+        }
+    }
+}
